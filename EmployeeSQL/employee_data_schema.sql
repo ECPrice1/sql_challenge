b1f2c2784298bdd7d12c38091f139c63dbde326a
ECPrice1 <ericprice@Erics-MacBook-Pro.local>
2024-04-21 17:49:17 -0600

    Updates to schema and DB diagram

--- EmployeeSQL/employee_data_schema.sql
+++ EmployeeSQL/employee_data_schema.sql
@@ -1,43 +1,46 @@
 CREATE TABLE departments(
-  dept_no VARCHAR(30) PRIMARY KEY,
-  dept_name VARCHAR
+  dept_no VARCHAR(30)   NOT NULL,
+  dept_name VARCHAR   NOT NULL,
+  PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE titles(
-	title_id VARCHAR(30) PRIMARY KEY,
-	title VARCHAR(30)
+	title_id VARCHAR(30)   NOT NULL,
+	title VARCHAR(30)   NOT NULL,
+    PRIMARY KEY (title_id)
 );
 
 CREATE TABLE employees(
-	emp_no SERIAL PRIMARY KEY,
-	emp_title_id VARCHAR(30),
-    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
-	birth_date DATE,
-	first_name VARCHAR(30),
-	last_name VARCHAR(30),
-	sex VARCHAR(30),
-	hire_date DATE
+	emp_no SERIAL    NOT NULL,
+	emp_title_id VARCHAR(30)   NOT NULL,
+	birth_date DATE   NOT NULL,
+	first_name VARCHAR(30)   NOT NULL,
+	last_name VARCHAR(30)   NOT NULL,
+	sex VARCHAR(30)   NOT NULL,
+	hire_date DATE   NOT NULL, 
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 CREATE TABLE dept_emp(
-	emp_no SERIAL,
+	emp_no SERIAL   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	dept_no VARCHAR (30),
+	dept_no VARCHAR (30)   NOT NULL,
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
-	dept_no VARCHAR(30),
+	dept_no VARCHAR(30)   NOT NULL,
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	emp_no SERIAL, 
+	emp_no SERIAL   NOT NULL, 
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 
 
 CREATE TABLE salaries(
-	emp_no SERIAL,
+	emp_no SERIAL   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	salary INT
+	salary INT   NOT NULL
 );
 
