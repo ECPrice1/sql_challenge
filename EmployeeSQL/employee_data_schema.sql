61a91139def56ee756da412c577d2996c68a0b79
ECPrice1 <ericprice@Erics-MacBook-Pro.local>
2024-04-22 13:17:17 -0600

    final schema

--- EmployeeSQL/employee_data_schema.sql
+++ EmployeeSQL/employee_data_schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE employees(
 	birth_date DATE   NOT NULL,
 	first_name VARCHAR(30)   NOT NULL,
 	last_name VARCHAR(30)   NOT NULL,
-	sex VARCHAR(30)   NOT NULL,
+	sex VARCHAR(1)   NOT NULL,
 	hire_date DATE   NOT NULL, 
     PRIMARY KEY (emp_no),
     FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
@@ -41,6 +41,7 @@ CREATE TABLE dept_manager(
 CREATE TABLE salaries(
 	emp_no SERIAL   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	salary INT   NOT NULL
+	salary INT   NOT NULL,
+    PRIMARY KEY (salary)
 );
 
